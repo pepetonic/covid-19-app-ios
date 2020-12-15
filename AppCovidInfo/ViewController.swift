a395999c85c187b826e3beea8c10cc25124f8ede
Francisco Olivares <olivaresabadfco10@gmail.com>
2020-12-15 16:28:05 -0600

    Interfaz modificada e inicio completado

--- AppCovidInfo/ViewController.swift
+++ AppCovidInfo/ViewController.swift
@@ -10,17 +10,22 @@ import WebKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelPais: UILabel!
+    @IBOutlet weak var labelTotalCasos: UILabel!
+    @IBOutlet weak var labelDefunciones: UILabel!
+    @IBOutlet weak var labelRecuperados: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Cargar Api al iniciar la app
-        cargarInfo()
+        cargarInfoPrincipal()
     }
     
-    func cargarInfo(){
-        
-        let urlAPI = URL(string: "https://corona.lmao.ninja/v3/covid-19/countries/")
+    func cargarInfoPrincipal(){
+        let urlAPI = URL(string: "https://corona.lmao.ninja/v3/covid-19/all")
+        //let urlAPI = URL(string: "https://corona.lmao.ninja/v3/covid-19/countries/mexico")
         
         let peticion = URLRequest(url: urlAPI!)
         
@@ -30,21 +35,32 @@ class ViewController: UIViewController {
                 print(error!)
             }else {
                 do{
+                    
                     let json = try JSONSerialization.jsonObject(with: datos!, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
                     
-                    /*
-                    let pais = json["country"] as! String
-                    //let stringPais = pais as! String
+                    //let pais = json["country"] as! String
+                    let casos = json["cases"] as! Int
+                    let muertes = json["deaths"] as! Int
+                    let recuperados = json["recovered"] as! Int
+                    
+                    //let subjson = json ["countryInfo"] as! [String: Any]
+                    //let img = subjson ["flag"] as! String
+                    let pais = "Global"
                     if pais == ""{
                         DispatchQueue.main.sync {
-                            let page = "<h1 style = 'font-size: 100px'>ESTADO GENERAL</h1><ul><li>\(pais)</li></ul>"
-                            self.webView.loadHTMLString(page, baseURL: nil)
+                            self.labelPais.text = "Error"
                         }
                     }else{
                         DispatchQueue.main.sync {
-                            self.webView.loadHTMLString("<h1>\(pais)</h1>", baseURL: nil)
+                            self.labelPais.text = "Stituación \(pais)"
+                            self.labelTotalCasos.text = "Total de casos: \(String(casos))"
+                            self.labelDefunciones.text = "Total de defunciones: \( String(muertes))"
+                            self.labelRecuperados.text = "Recuperados: \(String(recuperados))"
                         }
-                    }*/
+                                     
+                    
+                    }
+                    
                     
                 } catch{
                     print("Error al procesar el Json \(error.localizedDescription)")
@@ -55,7 +71,74 @@ class ViewController: UIViewController {
         tarea.resume()
     }
 
+    func buscarInfoPais(country: String){
+        let urlAPI = URL(string: "https://corona.lmao.ninja/v3/covid-19/countries/\(country)")
+        let peticion = URLRequest(url: urlAPI!)
+        let tarea = URLSession.shared.dataTask(with: peticion){datos,respuesta,error in
+            if error != nil {
+                print(error!)
+            }else {
+                do{
+                    let json = try JSONSerialization.jsonObject(with: datos!, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                    
+                    let pais = json["country"] as! String
+                    let casos = json["cases"] as! Int
+                    let muertes = json["deaths"] as! Int
+                    let recuperados = json["recovered"] as! Int
+                    
+                    //let subjson = json ["countryInfo"] as! [String: Any]
+                    //let img = subjson ["flag"] as! String
+                    
+                    if pais == ""{
+                        DispatchQueue.main.sync {
+                            self.labelPais.text = "Error"
+                        }
+                    }else{
+                        DispatchQueue.main.sync {
+                            self.labelPais.text = "Stituación \(pais)"
+                            self.labelTotalCasos.text = "Total de casos: \(String(casos))"
+                            self.labelDefunciones.text = "Total de defunciones: \( String(muertes))"
+                            self.labelRecuperados.text = "Recuperados: \(String(recuperados))"
+                        }
+                                     
+                    
+                    }
+                    
+                    
+                } catch{
+                    print("Error al procesar el Json \(error.localizedDescription)")
+                }
+            }
+            
+        }
+        tarea.resume()
+    }
     @IBAction func buscarPais(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Buscar país", message: "Escriba el nombre del país en Inglés", preferredStyle: .alert)
+        
+        alert.addTextField { (paisAlert) in
+            paisAlert.placeholder = "Pais"
+        }
+        
+        let actionAceptar = UIAlertAction(title: "Buscar", style: .default) { (_) in
+            guard let pais = alert.textFields?.first?.text else { return }
+            
+            if pais == ""{
+                let alerta = UIAlertController(title: "Error", message: "Llena el campo de texto", preferredStyle: .alert)
+                let aceptar = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+                alerta.addAction(aceptar)
+                self.present(alerta,animated: true, completion: nil)
+            }else {
+                
+            }
+        }
+        
+        let actionCancelar = UIAlertAction (title: "Cancelar", style: .default, handler: nil)
+        
+        alert.addAction(actionAceptar)
+        alert.addAction(actionCancelar)
+        
+        present(alert,animated: true, completion:nil)
     }
 }
 
